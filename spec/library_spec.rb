0b150bf4bb888036260480421cca4fe3bf6ad800
Thomas Hawley <thawley22@turing.edu>
2023-02-24 10:01:39 -0700

    test: Add #add_author tests

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -3,6 +3,11 @@ require 'spec_helper'
 RSpec.describe Library do
   before(:each) do
     @library = Library.new("Library of Alexandria")
+
+    @charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+
+    @jane_eyre = @charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    @villette = @charlotte_bronte.write("Villette", "1853")
   end
 
   describe '#initialize' do
@@ -20,6 +25,20 @@ RSpec.describe Library do
 
     it 'starts with an empty array of authors' do
       expect(@library.authors).to eq([])
+    end
+  end
+
+  describe '#add_author' do
+    it 'can add authors to the library' do
+      @library.add_author(@charlotte_bronte)
+
+      expect(@library.authors).to eq([@charlotte_bronte])
+    end
+
+    it 'when adding authors to library it also adds the authors books to the library' do
+      @library.add_author(@charlotte_bronte)
+
+      expect(@library.books).to eq([@jane_eyre, @villette])
     end
   end
 end
