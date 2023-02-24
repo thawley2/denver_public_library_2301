febed33993a1ab6cad0e26818277eb5da6c56779
Thomas Hawley <thawley22@turing.edu>
2023-02-24 10:24:03 -0700

    test: Update #publication_year test

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -3,6 +3,8 @@ require 'spec_helper'
 RSpec.describe Book do
   before(:each) do
     @book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+    @book2 = Book.new({author_first_name: "Thomas", author_last_name: "Hawley", title: "The universe", publication_date: "2023"})
+    
   end
 
   describe '#initialize' do
@@ -20,6 +22,10 @@ RSpec.describe Book do
 
     it 'has a publication year' do
       expect(@book.publication_year).to eq("1960")
+    end
+
+    it 'can still show publication year if only a year is provided' do
+      expect(@book2.publication_year).to eq("2023")
     end
   end
 end
