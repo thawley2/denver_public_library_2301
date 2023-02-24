6b0c96ef0c5fdea7da8c41307ee27c0f32e5e8e8
Thomas Hawley <thawley22@turing.edu>
2023-02-24 09:57:40 -0700

    Remove comment

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 RSpec.describe Author do
   before(:each) do
     @charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
-    # @jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    
   end
 
   describe '#initialize' do
