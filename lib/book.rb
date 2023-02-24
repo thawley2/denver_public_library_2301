0bd3c5f6496ee3635012a82784d1f8327dc2b420
Thomas Hawley <thawley22@turing.edu>
2023-02-24 09:12:17 -0700

    feat: Create Book class with initialize methods

--- lib/book.rb
+++ lib/book.rb
@@ -1,3 +1,18 @@
 class Book
+attr_reader :title
+
+  def initialize(book_info)
+    @title = book_info[:title]
+    @author_first_name = book_info[:author_first_name]
+    @author_last_name = book_info[:author_last_name]
+    @publication_date = book_info[:publication_date]
+  end
   
+  def author
+    "#{@author_first_name} #{@author_last_name}"
+  end
+
+  def publication_year
+    @publication_date.split(", ")[-1]
+  end
 end
