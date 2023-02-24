a54382f5a88c21204306ee93b6402a52e75d0e9b
Thomas Hawley <thawley22@turing.edu>
2023-02-24 09:39:34 -0700

    feat: Add #write method

--- lib/author.rb
+++ lib/author.rb
@@ -1,8 +1,23 @@
 class Author
-attr_reader :name, :books
+attr_reader :books
 
   def initialize(author_data)
-    @name = "#{author_data[:first_name]} #{author_data[:last_name]}"
+    @first_name = author_data[:first_name]
+    @last_name = author_data[:last_name]
     @books = []
+  end
+
+  def name
+    "#{@first_name} #{@last_name}"
+  end
+
+  def write(name_of_book, pub_date)
+    @books << new_book = Book.new({
+      author_first_name: @first_name, 
+      author_last_name: @last_name,
+      title: name_of_book,
+      publication_date: pub_date
+      })
+    new_book
   end
 end
