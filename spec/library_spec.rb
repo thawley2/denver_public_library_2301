dc42ba389d4a0e7f2d6a70257abf7ae0e0fd3223
Thomas Hawley <thawley22@turing.edu>
2023-02-24 09:48:01 -0700

    test: Add #initialize tests to Library class

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -1,1 +1,17 @@
 require 'spec_helper'
+
+RSpec.describe Library do
+  before(:each) do
+    @library = Library.new("Library of Alexandria")
+  end
+
+  describe '#initialize' do
+    it 'exists' do
+      expect(@library).to be_a(Library)
+    end
+
+    it 'has a name' do
+      expect(@library.name).to eq("Library of Alexandria")
+    end
+  end
+end
