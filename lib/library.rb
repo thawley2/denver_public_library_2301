64641fc7e1c1fb0abd3fe98a2afe41c388fd55ca
Thomas Hawley <thawley22@turing.edu>
2023-02-24 10:20:41 -0700

    feat: Add #publication_time_frame_for method

--- lib/library.rb
+++ lib/library.rb
@@ -11,4 +11,12 @@ class Library
   @authors << author
   author.books.each {|book| @books << book}
  end
+
+ def publication_time_frame_for(author)
+  publication_year_sorted = author.books.map do |book|
+    book.publication_year.to_i
+  end.sort
+  start_end_publish = {start: publication_year_sorted.first.to_s,
+  end: publication_year_sorted.last.to_s}
+ end
 end
